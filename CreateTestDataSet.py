9e0346e1e5bc72b54f864c096f4c92c8e9209ce0
Evan <1522910531@qq.com>
2020-09-26 17:45:53 +0800

    create Test data set

---
+++ CreateTestDataSet.py
@@ -0,0 +1,16 @@
+import hashlib
+import numpy as np
+
+
+# use a identifier to decide whether it can enter into the test data set
+# function digest() returns a binary string value
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256*test_ratio
+
+
+# split the test data set and the train data set
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
